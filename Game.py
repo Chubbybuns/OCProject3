a2d7033e4eee2280e8523b7d8581af7888a82245
Julia <julia.l171199@gmail.com>
2019-12-18 23:26:13 +0100

    arrêt de la boucle principale avec booleen

--- Game.py
+++ Game.py
@@ -34,6 +34,7 @@ finish_cell = maze.array[len(maze.array) // 20 * 15][len(maze.array) // 20 * 15]
 print(finish_cell)
 
 # Main loop
+gameover = False
 run = True
 while run:
     pygame.time.delay(30)
@@ -41,65 +42,64 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if macgyver.x == (len(macgyver.maze.array) * 20) - 20 and macgyver.y == (len(macgyver.maze.array) * 20) - 20:
+        elif macgyver.x == (len(macgyver.maze.array) * 20) - 20 and macgyver.y == (len(macgyver.maze.array) * 20) - 20:
             first_item = macgyver.item_list[0]
             if isinstance(first_item, Syringe):
-                print("ok")
-            print("not ok")
+                gameover = True
 
+    if not gameover:
+        keys = pygame.key.get_pressed()
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        macgyver.move_left()
-    elif keys[pygame.K_RIGHT]:
-        macgyver.move_right()
-    elif keys[pygame.K_UP]:
-        macgyver.move_up()
-    elif keys[pygame.K_DOWN]:
-        macgyver.move_down()
+        if keys[pygame.K_LEFT]:
+            macgyver.move_left()
+        elif keys[pygame.K_RIGHT]:
+            macgyver.move_right()
+        elif keys[pygame.K_UP]:
+            macgyver.move_up()
+        elif keys[pygame.K_DOWN]:
+            macgyver.move_down()
 
     # donner l'item à macgyver s'il y en a un
-    macgyver.get_item_from_current_cell()
-    macgyver.make_syringe_if_possible()
+        macgyver.get_item_from_current_cell()
+        macgyver.make_syringe_if_possible()
 
     # Pour tout réafficher
-    win.fill((0, 0, 0))
+        win.fill((0, 0, 0))
 
     # Maze and consumables display
 
-    for line_number, line in enumerate(maze.array, start=0):
-        y = line_number * 20
+        for line_number, line in enumerate(maze.array, start=0):
+            y = line_number * 20
 
-        for cell_number, cell in enumerate(line, start=0):
-            x = cell_number * 20
-            Img_path = cell.load_img()
-            Img = pygame.image.load(Img_path)
-            win.blit(Img, (x, y))
-            if isinstance(cell, Finish):
-                Img_path2 = guard.load_img()
-                Img2 = pygame.image.load(Img_path2)
-                win.blit(Img2, (x, y))
-
-            items = cell.get_items()
-            for item in items:
-                Img_path = item.load_img()
+            for cell_number, cell in enumerate(line, start=0):
+                x = cell_number * 20
+                Img_path = cell.load_img()
                 Img = pygame.image.load(Img_path)
                 win.blit(Img, (x, y))
+                if isinstance(cell, Finish):
+                    Img_path2 = guard.load_img()
+                    Img2 = pygame.image.load(Img_path2)
+                    win.blit(Img2, (x, y))
+
+                items = cell.get_items()
+                for item in items:
+                    Img_path = item.load_img()
+                    Img = pygame.image.load(Img_path)
+                    win.blit(Img, (x, y))
 
     # MacGyver Display
-    Img_path = macgyver.load_img()
-    Img = pygame.image.load(Img_path)
-    win.blit(Img, (macgyver.x, macgyver.y))
-
-    items_list = macgyver.get_items()
-    for item_number, item in enumerate(items_list, start=0):
-        Img_path = item.load_img()
+        Img_path = macgyver.load_img()
         Img = pygame.image.load(Img_path)
-        win.blit(Img, (10 + item_number * (20 + 20), len(maze.array) * 20 + 5))
+        win.blit(Img, (macgyver.x, macgyver.y))
+
+        items_list = macgyver.get_items()
+        for item_number, item in enumerate(items_list, start=0):
+            Img_path = item.load_img()
+            Img = pygame.image.load(Img_path)
+            win.blit(Img, (10 + item_number * (20 + 20), len(maze.array) * 20 + 5))
 
     # Window update
-    pygame.display.update()
+        pygame.display.update()
 
 # Game exit
 pygame.quit()
